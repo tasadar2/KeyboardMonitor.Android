3f66569cd78c58c8904de652f298fcb8d4c8bc53
tasadar2 <tassadar@wowway.com>
2015-09-11 00:41:52 -0400

    Added idle logic

--- app/src/main/java/com/transpiria/keyboardmonitor/StatisticsService.java
+++ app/src/main/java/com/transpiria/keyboardmonitor/StatisticsService.java
@@ -1,7 +1,6 @@
 package com.transpiria.keyboardmonitor;
 
-import android.telephony.SubscriptionManager;
-import android.widget.TextView;
+import android.os.Handler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -11,6 +10,7 @@ import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.TimerTask;
 
 public class StatisticsService
         implements
@@ -21,13 +21,26 @@ public class StatisticsService
         void SubscriptionsChanged();
     }
 
+    public interface IStateChanged {
+        void StateChanged(State state);
+    }
+
+    public enum State {
+        Normal,
+        Idle;
+    }
+
     private Communicator Communicator;
     private Subscription Subscription;
     private Subscription LastSubscription;
+    private Handler IdleHandler;
+    private Date LastDiscoverCheck;
 
     public List<Subscription> Subscriptions;
+    public State IdleState = State.Normal;
 
     public final Event<ISubscriptionsChanged> SubscriptionsChangedEvent = new Event<>();
+    public final Event<IStateChanged> StateChangedEvent = new Event<>();
 
     public StatisticsService() {
         Subscriptions = new ArrayList<>();
@@ -35,8 +48,48 @@ public class StatisticsService
         Communicator = new Communicator();
         Communicator.EndpointDiscoveredEvent.AddObserver(this);
         Communicator.IMessageReceivedEvent.AddObserver(this);
-        Communicator.Discover(27831);
+
+        IdleHandler = new Handler();
+        SetActive();
     }
+
+    private void Discover() {
+        if (LastDiscoverCheck == null || LastDiscoverCheck.getTime() + 10 * 1000 < new Date().getTime()) {
+            LastDiscoverCheck = new Date();
+            Communicator.Discover(27831);
+        }
+    }
+
+    public void SetActive() {
+        IdleHandler.postDelayed(CheckIdle, 0);
+    }
+
+    public void SetInactive() {
+        IdleHandler.removeCallbacks(CheckIdle);
+        UnSubscribe();
+    }
+
+    public TimerTask CheckIdle = new TimerTask() {
+        @Override
+        public void run() {
+            boolean idle = Subscription == null || Subscription.LastReceived.getTime() + 3 * 1000 < new Date().getTime();
+            if (idle && IdleState != State.Idle) {
+                ZeroValues();
+                OnStateChanged(State.Idle);
+            } else if (!idle && IdleState != State.Normal) {
+                OnStateChanged(State.Normal);
+            }
+
+            if (idle &&
+                    (LastSubscription == null || LastSubscription.LastSeen.getTime() + 10 * 1000 < new Date().getTime()) &&
+                    (LastDiscoverCheck == null || LastDiscoverCheck.getTime() + 10 * 1000 < new Date().getTime())) {
+                UnSubscribe();
+                Discover();
+            }
+
+            IdleHandler.postDelayed(CheckIdle, 1000);
+        }
+    };
 
     public boolean isSubscribed() {
         return Subscription != null;
@@ -70,11 +123,12 @@ public class StatisticsService
         long now = new Date().getTime();
         long subscriptionExpiration = 5 * 60 * 1000;
         boolean subscriptionsChanged = false;
-        for (Subscription subscription : Subscriptions) {
+        for (int index = Subscriptions.size() - 1; index > -1; index--) {
+            Subscription subscription = Subscriptions.get(index);
             if (subscription.Address == ipAddress && subscription.Port == port) {
                 newSubscription = subscription;
             } else if (now - subscription.LastSeen.getTime() > subscriptionExpiration) {
-                Subscriptions.remove(subscription);
+                Subscriptions.remove(index);
                 subscriptionsChanged = true;
             }
         }
@@ -101,6 +155,13 @@ public class StatisticsService
         }
     }
 
+    protected void OnStateChanged(State state) {
+        IdleState = state;
+        for (IStateChanged observer : StateChangedEvent.Observers) {
+            observer.StateChanged(state);
+        }
+    }
+
     @Override
     public void MessageReceived(byte[] content) {
         try {
@@ -119,6 +180,21 @@ public class StatisticsService
             Current = info;
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private void ZeroValues() {
+        if (Current != null) {
+            ZeroCounter(Current.Processor);
+            ZeroCounter(Current.BytesReceived);
+            ZeroCounter(Current.BytesSent);
+        }
+    }
+
+    private void ZeroCounter(Counter counter) {
+        counter.Value = 0;
+        for (int index = 0; index < counter.Values.size(); index++) {
+            counter.Values.set(index, 0.0);
         }
     }
 
